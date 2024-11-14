8a71934433ce8d7f876c9a8d853b6929cee42642
Mohamed-elhediKraiem <113349583+Mohamed-elhediKraiem@users.noreply.github.com>
2024-11-14 10:52:38 +0100

    ajout clerck

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,15 +4,35 @@ export const metadata = {
   title: 'MapGpt',
   description: 'Un ChatGpt pour trouver ton itinéraire !',
 }
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
+import './globals.css'
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <div className='absolute z-50 bg-red-500'>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+          
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
