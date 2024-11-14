bdd6f6c67271fe90e18466dbcd0ea00dafdc69b3
Mohamed-elhediKraiem <113349583+Mohamed-elhediKraiem@users.noreply.github.com>
2024-11-14 17:16:17 +0100

    ajout pprisma avec la fonction createHistorique

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,17 +1,12 @@
-import './globals.css'
+// 'use client'
 
+import './globals.css'
+import {ClerkProvider} from '@clerk/nextjs'
 export const metadata = {
   title: 'MapGpt',
   description: 'Un ChatGpt pour trouver ton itinéraire !',
 }
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
-import './globals.css'
+
 export default function RootLayout({
   children,
 }: {
@@ -21,15 +16,6 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body>
-          <div className='absolute z-50 bg-red-500'>
-            <SignedOut>
-              <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </div>
-          
           {children}
         </body>
       </html>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,39 +1,18 @@
 "use client";
 
-import ResultCard from "./(components)/resultcard";
-import Map from "./(components)/map";
 import { useState, useEffect } from "react";
-import useChatBar from "./(components)/chatbar";
-import { processPathfinding } from "./context/requestsUtils";
-
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const [mapData, setMapData] = useState({
-    algorithm: "AStar",
-    distance: 166.47,
-    explored_nodes: 13,
-    average_node_time: 0.00030765166649451625,
-    memory_usage: 0,
-    path: ["La Douzillère", "Joué-lès-Tours", "Bressuire", "Chalonnes"],
-    path_length: 4,
-    tries: 13,
-    time: 0.003999471664428711,
-    
-  });
-  const handleSubmit = async () => {
-    const optimalPath = await processPathfinding(prompt);
-    setMapData(optimalPath.pathfinding_result)
-  };
-  const {chatbar, prompt} = useChatBar({onClick : handleSubmit}); 
+  const router = useRouter()
 
+  useEffect(() => {
+    router.push('/home')
+  }, [])
   return (
     <>
-      <div className="absolute">
-        <Map mapUrl={mapData.map_url || "/assets/map.html" } />
-      </div>
-      <div className="w-full min-h-screen p-20 flex flex-col justify-between z-10">
-        <ResultCard mapData={mapData} />
-        {chatbar}
+      <div className="bg-black h-full w-full flex justify-center items-center">
+          <h1 className="text-white">Redirection vers la page d'accueil</h1>
       </div>
     </>
   );
