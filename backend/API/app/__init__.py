0b4023f26183eee10cf5445d70fb793a5a138972
sanlamamba <contact@sanlamamba.com>
2025-02-01 20:18:02 +0100

    feat(app): enable CORS support and update requirements for new dependencies

--- backend/API/app/__init__.py
+++ backend/API/app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import config
 from .extensions import api
 from .routes import register_routes
+from flask_cors import CORS
 
 
 def create_app(config_name):
     app = Flask(__name__, static_folder="static", static_url_path="/static")
+    CORS(app)
     app.config.from_object(config[config_name])
 
     api.init_app(app)
