43f5361e896806e14b71c311b3cce19fb8c92837
sanlamamba <contact@sanlamamba.com>
2025-02-01 20:01:59 +0100

    refactor(app): clean up imports and update configuration formatting

--- backend/API/app/__init__.py
+++ backend/API/app/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from config import config
-from .extensions import db, migrate, api
+from .extensions import api
 from .routes import register_routes
 
+
 def create_app(config_name):
-    app = Flask(__name__, static_folder='static', static_url_path='/static')
+    app = Flask(__name__, static_folder="static", static_url_path="/static")
     app.config.from_object(config[config_name])
 
-    db.init_app(app)
-    migrate.init_app(app, db)
     api.init_app(app)
 
     register_routes(api)
