c22c4ee4256d2e0fd3e72908372889d82ccdec6a
sanlamamba <contact@sanlamamba.com>
2025-02-02 12:41:41 +0100

    refactor(ner_service): removed unused print

--- backend/API/app/services/ner_service.py
+++ backend/API/app/services/ner_service.py
@@ -13,7 +13,9 @@ def extract_entities(text):
 
     for res in inferences:
         search_result = CityToTrainHelper.fuzzy_search(res["place"])
-        closest_train_station = CityToTrainHelper.get_closest_train_station(search_result["name"])
+        closest_train_station = CityToTrainHelper.get_closest_train_station(
+            search_result["name"]
+        )
         res["train_station"] = closest_train_station
         res["place"] = search_result["name"]
 
@@ -22,13 +24,12 @@ def extract_entities(text):
     for item in inferences:
         new_item = {}
         for key, value in item.items():
-            print(key, value)
             if key == "status" and value == "departure":
                 new_item["status"] = "start"
             elif key == "status" and value == "arrival":
                 new_item["status"] = "end"
             elif key == "status" and value == "intermediary":
-                new_item["status"] = "intermediate" 
+                new_item["status"] = "intermediate"
             elif key == "train_station":
                 new_item["station"] = value
         response.append(new_item)
